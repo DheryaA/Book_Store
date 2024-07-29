7f72270fa10c331d6475629f44ae9722a22f9f5d
DeepakBisht12 <92213746+DheryaA@users.noreply.github.com>
2024-07-30 05:02:05 +0530

    Update AllOrders.jsx

--- frontend/frontend/src/pages/AllOrders.jsx
+++ frontend/frontend/src/pages/AllOrders.jsx
@@ -23,7 +23,7 @@ const AllOrders = () => {
     useEffect(() => {
         const fetch = async () => {
             const response = await axios.get(
-                "http://localhost:1000/api/v1/get-all-orders",
+                "https://book-store-8uzx.onrender.com/api/v1/get-all-orders",
                 { headers }
             );
             setAllOrders(response.data.data);
@@ -39,7 +39,7 @@ const AllOrders = () => {
     const submitChanges = async (i) => {
         const id = AllOrders[i]._id;
         const response = await axios.put(
-            `http://localhost:1000/api/v1/update-status/${id}`,
+            `https://book-store-8uzx.onrender.com/api/v1/update-status/${id}`,
             Values,
             { headers }
         );
